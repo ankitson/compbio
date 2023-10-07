7f45efb116598fd0b2a947fc6e795b6c6902944e
Ankit Soni <ankitsoni9@gmail.com>
2023-10-07 14:51:05 -0700

    improve layout

--- dashweb/dashapp.py
+++ dashweb/dashapp.py
@@ -26,12 +26,22 @@ def build_layout():
       dbc.Row(html.H1(children='Bioinformatics I', style={'textAlign':'center'}),),
       dbc.Row([
         dbc.Col(dcc.Dropdown(options=figure_select, value=figure_select[1], id='dropdown-selection')),
-        dbc.Col(dcc.RadioItems(options=["A","T","C","G"], value="C", id="nucleotide-selection",  inline=True)),
-        dbc.Col(dcc.RadioItems(options=["A","T","C","G"], value="C", id="nucleotide-selection2",  inline=True)),
+        dbc.Col(dbc.Stack([
+          dcc.RadioItems(options=["A","T","C","G"], value="G", id="baseradio1",  inline=True),
+          dcc.RadioItems(options=["A","T","C","G"], value="C", id="baseradio2",  inline=True)
+        ])),
       ]),
       dbc.Row([
-        dbc.Col([dcc.Markdown("## Plot", style={'textAlign':'center'}), dcc.Graph(figure={}, id='graph-content')],width=6),
-        dbc.Col([dcc.Markdown("## Description", style={'textAlign':'center'}), dcc.Markdown(id='graph-description', style={'textAlign':'center'}, dangerously_allow_html=True)], width=6),
+        dbc.Col([
+          dcc.Markdown("## Plot", style={'textAlign':'center'}), 
+          dcc.Graph(figure={}, id='graph-content', className='border-top border-start border-end border-primary')
+        ],width=6),
+        dbc.Col([
+          dcc.Markdown("## Description", style={'textAlign':'center'}), 
+          dcc.Markdown(
+            id='graph-description', style={'textAlign':'center'}, 
+            className='border-top border-secondary', dangerously_allow_html=True)
+        ], width=6),
       ], className='g-0')
     ], fluid=True)
 
@@ -39,9 +49,10 @@ def init_callbacks(dash_app):
   @dash_app.callback(
     Output('graph-content', 'figure'),
     Output('graph-description', 'children'),
+    Output('baseradio2', 'style'),
     Input('dropdown-selection', 'value'),
-    Input("nucleotide-selection", 'value'),
-    Input("nucleotide-selection2", 'value')
+    Input("baseradio1", 'value'),
+    Input("baseradio2", 'value')
   )
   def update_graph(value, nvalue, nvalue2):
     if value == 'Counts (Raw)':
@@ -57,5 +68,9 @@ def init_callbacks(dash_app):
     else:
       (fig,descr) = (px.bar(x=[1,2,3],y=[1,2,3]), """This is an example <br/>""" )
     
-    fig.update_layout(height=559, margin=dict(l=0,r=0,b=0))
-    return (fig, descr)
+    style = { 'display' : 'none' } 
+    if value == "Diff Counts":
+      style = { 'display' : 'block' }
+
+    fig.update_layout(height=559, margin=dict(l=0,t=40,r=0,b=0))
+    return (fig, descr, style)
